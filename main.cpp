8233552a73b6ef2a902ffcff024727499cb51526
yitendra <kumar.yitendra@gmail.com>
2017-10-06 11:53:58 +0530

    Added color

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
-#include <iostream>
-#include <fstream>
-#include <string.h>
+#include <bits/stdc++.h>
+#include <windows.h>
 
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   //Used printing in color
+//color code=> 242 for green and 252 for red and 240 for black.
 using namespace std;
+void printInGreen(string);
+void printInRed(string);
 
 int main()
 {
-	ifstream file1("file1.txt");
-	ifstream file2("file2.txt");
-	string f1,f2;int a;
-	while(file1>>f1 && file2>>f2){
-        if(f1==f2){cout<<"Match"<<endl;}else{cout<<"No Match\n";}
-	}
+
+system("COLOR f0");
+
+	ifstream file1("file1.txt"),file2("file2.txt");
+	string f1,f2;
+	while(getline(file1,f1) && getline(file2,f2)){
+        if(f1==f2){printInRed("Match");}
+        else{
+               printInGreen("No match");
+        }}
+        cout<<"Testing";
+
+        return 0;
+
+}
+
+void printInGreen(string a){
+    SetConsoleTextAttribute(console, 242);
+    cout<<a<<endl;
+    SetConsoleTextAttribute(console, 240);
+}
+void printInRed(string a){
+    SetConsoleTextAttribute(console, 252);
+    cout<<a<<endl;
+    SetConsoleTextAttribute(console, 240);
 }
