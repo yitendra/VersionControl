a8268b8501f9c48e7af9198f5a333c667cd29655
sishubjoshi <shisub1999@gmail.com>
2017-10-15 21:09:12 +0530

    added UI

---
+++ data/Compare.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h>
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   //Used printing in color
+//color code=> 242 for green and 252 for red and 240 for black.
+using namespace std;
+void printInGreen(string);
+void printInRed(string);
+void compare(string *a, string *b);
+
+int main()
+{
+
+system("COLOR f0");
+
+	ifstream file1("old.txt"),file2("new.txt");
+	string f1,f2,temp1[100],temp2[100];
+	int i=1,j=1;
+	while(getline(file1,f1)){
+        if(f1!=""){temp1[i]=f1;i++;}
+        }
+    while(getline(file2,f2)){
+        if(f2!=""){temp2[j]=f2;j++;}
+    }
+compare(temp1,temp2);
+    getch();
+}
+
+void printInGreen(string a){
+    SetConsoleTextAttribute(console, 242);
+    a.insert(0,1,'+');
+    cout<<a<<endl;
+    SetConsoleTextAttribute(console, 240);
+}
+void printInRed(string a){
+    SetConsoleTextAttribute(console, 252);
+    a.insert(0,1,'-');
+    cout<<a<<endl;
+    SetConsoleTextAttribute(console, 240);
+}
+void compare(string *a, string *b){
+    int i=1,j=1;
+    while(a[i]!="" && b[j]!=""){
+        if(a[i]==b[j]){
+            cout<<a[i]<<endl;i++;j++;
+        }else{
+            int scan =j; bool found=0;
+            while(b[scan]!=""){
+                if(a[i]==b[scan]){found=1;break;}
+                scan++;
+            }
+            if(!found){
+                    printInRed(a[i]);i++;
+                }else{
+                    for(j; j<scan; j++){
+                        printInGreen(b[j]);
+                        }
+                    }
+        }
+            if(a[i]=="" && b[j]!=""){
+                printInGreen(b[j]);j++;
+            }
+    }
+}
+
--- main.cpp
+++ main.cpp
@@ -1,38 +1,98 @@
 #include <bits/stdc++.h>
 #include <windows.h>
-
-HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   //Used printing in color
-//color code=> 242 for green and 252 for red and 240 for black.
+#include <conio.h>
 using namespace std;
-void printInGreen(string);
-void printInRed(string);
+//declarations
+string disp,command;
+void compare();
+void editFile();
+void saveFile();
+bool isSame();
+void show();
+void check();
+
 
 int main()
 {
+    show();
+     /*isSame();
+    system("del old.txt")*/
 
-system("COLOR f0");
-
-	ifstream file1("file1.txt"),file2("file2.txt");
-	string f1,f2;int count=1;
-	while(getline(file1,f1) && getline(file2,f2)){
-        if(f1==f2){cout<<count<<" ";cout<<f1<<endl;}
-        else{
-               cout<<count<<" ";cout<<"(-)"<<f1<<endl<<count<<" (+)"<<f2<<endl;
-        }
-        count++;}
-        cout<<"Testing";
-
-        return 0;
-
+    system("main.bat");
+    return 0;
 }
 
-void printInGreen(string a){
-    SetConsoleTextAttribute(console, 242);
-    cout<<a<<endl;
-    SetConsoleTextAttribute(console, 240);
+void compare(){
+    ofstream file("main.bat");
+    file<<"echo off\ncls\ncd data\ncompare.exe";
 }
-void printInRed(string a){
-    SetConsoleTextAttribute(console, 252);
-    cout<<a<<endl;
-    SetConsoleTextAttribute(console, 240);
+
+void editFile(){
+   system("notepad file.txt");
+}
+
+bool isSame(){
+    system("copy data\\old.txt");
+    system("cls");
+    ifstream file("file.txt"),old("old.txt");
+    string a,b;bool flag=true;
+    while(getline(file,a) && getline(old,b)){
+        if(a!=b){return false;
+        flag = false;
+
+        break;}
+    }
+    return flag;
+}
+
+void saveFile(){
+    if(isSame()){
+        cout<<"No Changes were made.\n";
+    }else{
+        //save old to log++.txt
+
+        //save new to old.txt
+
+        //save file to new.txt
+    }
+}
+void show(){
+
+
+    ifstream doc("show.txt");
+    while(getline(doc,disp)){
+        cout<<disp<<endl;
+    }doc.close();
+    l:
+    cout<<endl<<"~$ ";
+    cin>>command;
+    if(command=="Start"){
+        system("cls");
+    }else{
+        cout<<"'"<<command<<"' is not recognized as an internal or external command,\noperable program or batch file."<<endl;
+        goto l;
+    }
+    ifstream help("help.txt");
+    while(getline(help,disp))
+        {
+            cout<<disp<<endl;
+
+        }help.close();
+        check();
+
+
+
+
+
+}
+void check(){s:
+    cout<<endl<<"~$ ";
+        cin>>command;
+
+    if(command=="compare")compare();
+    else if(command=="edit")editFile();
+    else if(command=="save")saveFile();
+    else {cout<<"'"<<command<<"' is not recognized as an internal or external command,\noperable program or batch file."<<endl;
+    goto s;}
+
 }
